eac27a1a5ee4b4ec615864bb0384f7f5b19f1d12
ar0ne <camtbitakou@yandex.ru>
2022-06-15 23:34:44 +0800

    added all instructions

--- main.py
+++ main.py
@@ -208,23 +208,359 @@ class CHIP8Interpreter:
         """
         code = self.op_code & 0xF0FF
         if code == 0x00E0:
-            self._00e0()
+            self._00E0()
         elif code == 0x00EE:
-            self._00ee()
+            self._00EE()
         else:
             self._0nnn()
 
-    def _00e0(self) -> None:
-        """CLS - clear screen"""
+    def _00E0(self) -> None:
+        """
+        00E0 - CLS
+        Clear the display.
+        """
         self.display = [0] * 64 * 32
         self.drawing = True
 
-    def _00ee(self) -> None:
-        """RET - return from a subroutine"""
+    def _00EE(self) -> None:
+        """
+        00EE - RET
+        Return from a subroutine.
+
+        The interpreter sets the program counter to the address at the top of the stack, then
+        subtracts 1 from the stack pointer.
+        """
         self.program_counter = self.stack.pop()
 
     def _0nnn(self) -> None:
-        """Jump to a machine code routine at nnn"""
+        """
+        0nnn - SYS addr
+        Jump to a machine code routine at nnn.
+
+        This instruction is only used on the old computers on which Chip-8 was originally
+        implemented. It is ignored by modern interpreters.
+        """
+        nnn = self.op_code & 0x0FFF
+        self.program_counter = nnn
+
+    def _1nnn(self) -> None:
+        """
+        1nnn - JP addr
+        Jump to location nnn.
+
+        The interpreter sets the program counter to nnn.
+        """
+        nnn = self.op_code & 0x0FFF
+        self.program_counter = nnn
+
+    def _2nnn(self) -> None:
+        """
+        2nnn - CALL addr
+        Call subroutine at nnn.
+
+        The interpreter increments the stack pointer, then puts the current PC on the top of the
+        stack. The PC is then set to nnn.
+        """
+        self.stack.append(self.program_counter)
+        nnn = self.op_code & 0x0FFF
+        self.program_counter = nnn
+
+    def _3xkk(self) -> None:
+        """
+        SE Vx, byte - Skip next instruction if Vx = kk
+        The interpreter compares register Vx to kk, and if they are equal, increments the program
+        counter by 2.
+        """
+        # TODO: is it correct?
+        kk = self.op_code & 0x00FF
+        Vx = self.op_code & 0x0F00  # how to get which v0-VF it is?
+        if kk == Vx:
+            self.program_counter += 2
+
+    def _4xkk(self) -> None:
+        """
+        SNE Vx, byte - skip next instruction if Vx != kk.
+        The interpreter compares register Vx to kk, and if they are not equal, increments the
+        program counter by 2.
+        """
+        # TODO
+
+    def _5xy0(self) -> None:
+        """
+        5xy0 - SE Vx, Vy
+        Skip next instruction if Vx = Vy.
+
+        The interpreter compares register Vx to register Vy, and if they are equal, increments
+        the program counter by 2.
+        """
+        # TODO
+
+    def _6xkk(self) -> None:
+        """
+        6xkk - LD Vx, byte
+        Set Vx = kk.
+
+        The interpreter puts the value kk into register Vx.
+        """
+        # TODO
+
+    def _7xkk(self) -> None:
+        """
+        7xkk - ADD Vx, byte
+        Set Vx = Vx + kk.
+
+        Adds the value kk to the value of register Vx, then stores the result in Vx.
+        """
+        # TODO
+
+    def _8xy0(self) -> None:
+        """
+        8xy0 - LD Vx, Vy
+        Set Vx = Vy.
+
+        Stores the value of register Vy in register Vx.
+        """
+        # TODO
+
+    def _8xy1(self) -> None:
+        """
+        8xy1 - OR Vx, Vy
+        Set Vx = Vx OR Vy.
+
+        Performs a bitwise OR on the values of Vx and Vy, then stores the result in Vx. A bitwise
+        OR compares the corresponding bits from two values, and if either bit is 1, then the same
+        bit in the result is also 1. Otherwise, it is 0.
+        """
+        # TODO
+
+    def _8xy2(self) -> None:
+        """
+        8xy2 - AND Vx, Vy
+        Set Vx = Vx AND Vy.
+
+        Performs a bitwise AND on the values of Vx and Vy, then stores the result in Vx. A bitwise
+        AND compares the corresponding bits from two values, and if both bits are 1, then the same
+        bit in the result is also 1. Otherwise, it is 0.
+        """
+        # TODO
+
+    def _8xy3(self) -> None:
+        """
+        8xy3 - XOR Vx, Vy
+        Set Vx = Vx XOR Vy.
+
+        Performs a bitwise exclusive OR on the values of Vx and Vy, then stores the result in Vx.
+        An exclusive OR compares the corresponding bits from two values, and if the bits are not
+        both the same, then the corresponding bit in the result is set to 1. Otherwise, it is 0.
+        """
+        # TODO
+
+    def _8xy4(self) -> None:
+        """
+        8xy4 - ADD Vx, Vy
+        Set Vx = Vx + Vy, set VF = carry.
+
+        The values of Vx and Vy are added together. If the result is greater than 8 bits
+        (i.e., > 255,) VF is set to 1, otherwise 0. Only the lowest 8 bits of the result are kept,
+        and stored in Vx.
+        """
+        # TODO
+
+    def _8xy5(self) -> None:
+        """
+        8xy5 - SUB Vx, Vy
+        Set Vx = Vx - Vy, set VF = NOT borrow.
+
+        If Vx > Vy, then VF is set to 1, otherwise 0. Then Vy is subtracted from Vx, and the
+        results stored in Vx.
+        """
+        # TODO
+
+    def _8xy6(self) -> None:
+        """
+        8xy6 - SHR Vx {, Vy}
+        Set Vx = Vx SHR 1.
+
+        If the least-significant bit of Vx is 1, then VF is set to 1, otherwise 0. Then Vx is
+        divided by 2.
+        """
+        # TODO
+
+    def _8xy7(self) -> None:
+        """
+        8xy7 - SUBN Vx, Vy
+        Set Vx = Vy - Vx, set VF = NOT borrow.
+
+        If Vy > Vx, then VF is set to 1, otherwise 0. Then Vx is subtracted from Vy, and the results
+        stored in Vx.
+        """
+        # TODO
+
+    def _8xyE(self) -> None:
+        """
+        8xyE - SHL Vx {, Vy}
+        Set Vx = Vx SHL 1.
+
+        If the most-significant bit of Vx is 1, then VF is set to 1, otherwise to 0. Then Vx is
+        multiplied by 2.
+        """
+        # TODO
+
+    def _9xy0(self) -> None:
+        """
+        9xy0 - SNE Vx, Vy
+        Skip next instruction if Vx != Vy.
+
+        The values of Vx and Vy are compared, and if they are not equal, the program counter is
+        increased by 2.
+        """
+        # TODO
+
+    def _Annn(self) -> None:
+        """
+        Annn - LD I, addr
+        Set I = nnn.
+
+        The value of register I is set to nnn.
+        """
+        # TODO
+
+    def _Bnnn(self) -> None:
+        """
+        Bnnn - JP V0, addr
+        Jump to location nnn + V0.
+
+        The program counter is set to nnn plus the value of V0.
+        """
+        # TODO
+
+    def _Cxkk(self) -> None:
+        """
+        Cxkk - RND Vx, byte
+        Set Vx = random byte AND kk.
+
+        The interpreter generates a random number from 0 to 255, which is then ANDed with the value
+         kk. The results are stored in Vx. See instruction 8xy2 for more information on AND.
+        """
+        # TODO
+
+    def _Dxyn(self) -> None:
+        """
+        Dxyn - DRW Vx, Vy, nibble
+        Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
+
+        The interpreter reads n bytes from memory, starting at the address stored in I. These bytes
+        are then displayed as sprites on screen at coordinates (Vx, Vy). Sprites are XORed onto the
+        existing screen. If this causes any pixels to be erased, VF is set to 1, otherwise it is set
+        to 0. If the sprite is positioned so part of it is outside the coordinates of the display,
+        it wraps around to the opposite side of the screen. See instruction 8xy3 for more
+        information on XOR, and section 2.4, Display, for more information on the Chip-8 screen
+        and sprites.
+        """
+        # TODO
+
+    def _Ex9E(self) -> None:
+        """
+        Ex9E - SKP Vx
+        Skip next instruction if key with the value of Vx is pressed.
+
+        Checks the keyboard, and if the key corresponding to the value of Vx is currently in the
+        down position, PC is increased by 2.
+        """
+        # TODO
+
+    def _ExA1(self) -> None:
+        """
+        ExA1 - SKNP Vx
+        Skip next instruction if key with the value of Vx is not pressed.
+
+        Checks the keyboard, and if the key corresponding to the value of Vx is currently in the up
+        position, PC is increased by 2.
+        """
+        # TODO
+
+    def _Fx07(self) -> None:
+        """
+        Fx07 - LD Vx, DT
+        Set Vx = delay timer value.
+
+        The value of DT is placed into Vx.
+        """
+        # TODO
+
+    def _Fx0A(self) -> None:
+        """
+        Fx0A - LD Vx, K
+        Wait for a key press, store the value of the key in Vx.
+
+        All execution stops until a key is pressed, then the value of that key is stored in Vx.
+        """
+        # TODO
+
+    def _Fx15(self) -> None:
+        """
+        Fx15 - LD DT, Vx
+        Set delay timer = Vx.
+
+        DT is set equal to the value of Vx.
+        """
+        # TODO
+
+    def _Fx18(self) -> None:
+        """
+        Fx18 - LD ST, Vx
+        Set sound timer = Vx.
+
+        ST is set equal to the value of Vx.
+        """
+        # TODO
+
+    def Fx1E(self) -> None:
+        """
+        Fx1E - ADD I, Vx
+        Set I = I + Vx.
+
+        The values of I and Vx are added, and the results are stored in I.
+        """
+        # TODO
+
+    def Fx29(self) -> None:
+        """
+        Fx29 - LD F, Vx
+        Set I = location of sprite for digit Vx.
+
+        The value of I is set to the location for the hexadecimal sprite corresponding to the value
+        of Vx. See section 2.4, Display, for more information on the Chip-8 hexadecimal font.
+        """
+        # TODO
+
+    def Fx33(self) -> None:
+        """
+        Fx33 - LD B, Vx
+        Store BCD representation of Vx in memory locations I, I+1, and I+2.
+
+        The interpreter takes the decimal value of Vx, and places the hundreds digit in memory at
+        location in I, the tens digit at location I+1, and the ones digit at location I+2.
+        """
+        # TODO
+
+    def Fx55(self) -> None:
+        """
+        Fx55 - LD [I], Vx
+        Store registers V0 through Vx in memory starting at location I.
+
+        The interpreter copies the values of registers V0 through Vx into memory, starting at the
+        address in I.
+        """
+        # TODO
+
+    def Fx65(self) -> None:
+        """
+        Fx65 - LD Vx, [I]
+        Read registers V0 through Vx from memory starting at location I.
+
+        The interpreter reads values from memory starting at location I into registers V0 through Vx
+        """
         # TODO
 
 
