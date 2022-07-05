8e3819ac750cd6f1c36db484734ac8059d47e401
ar0ne <camtbitakou@yandex.ru>
2022-07-05 17:19:42 +0800

    improve scenes

--- menu/menu_scene.py
+++ menu/menu_scene.py
@@ -31,10 +31,14 @@ class MenuScene(Scene):
         self.font = font.RetroFont("menu/data/nes-font.png", (8, 8))
         self.roms = self.read_files_from_folder(rom_dir)
         self.roms.sort()
-        self.page_size = 10
-        self.current_page = 1
+        self.page_size = 9
+        self.current_page = 0
         self.rom_buttons = []
         self.selected_rom_index = 0
+        self.pages = [
+            self.font.render(f"Page: {page + 1}")
+            for page in range(len(self.roms) // self.page_size + 1)
+        ]
         for rom_name in self.roms:
             btn_image = self.font.render(rom_name[:-4])
             btn_image_scaled = pygame.transform.scale2x(btn_image)
@@ -67,6 +71,7 @@ class MenuScene(Scene):
             ),
         )
         self.draw_game_select_buttons()
+        self.window.blit(self.pages[self.current_page], (10, 10))
 
     def draw_game_select_buttons(self) -> None:
         """Draw game buttons"""
@@ -75,10 +80,9 @@ class MenuScene(Scene):
         second_column_x_offset = self.window.get_width() / 2
         btn_border_margin = 4
         btn_page = self.rom_buttons[
-            (self.current_page - 1)
-            * self.page_size : self.page_size
-            * self.current_page
-            - 1
+            self.current_page
+            * self.page_size : (self.current_page + 1)
+            * self.page_size
         ]
         btn_page.append(self.next_page_button)
 
@@ -111,17 +115,28 @@ class MenuScene(Scene):
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_DOWN:
                     self.selected_rom_index += 1
-                    if self.selected_rom_index == 10:
+                    if self.selected_rom_index > self.page_size:
                         self.selected_rom_index = 0
                 if event.key == pygame.K_UP:
                     self.selected_rom_index -= 1
                     if self.selected_rom_index < 0:
-                        self.selected_rom_index = 9
+                        self.selected_rom_index = self.page_size
                 if event.key in (pygame.K_SPACE, pygame.K_RETURN):
-                    if self.selected_rom_index != 9:
-                        self.goToScene(GAME_SCENE_KEY)
-                    else:
+                    if not self.selected_rom_index % 10:
+                        # self.goToScene(GAME_SCENE_KEY)
                         pass
+                    else:
+                        self.current_page += 1
+                        self.selected_rom_index = (
+                            self.selected_rom_index
+                            if self.current_page * (self.page_size + 1) < len(self.roms)
+                            else 0
+                        )
+                        last_page = (
+                            self.current_page == len(self.roms) // self.page_size + 1
+                        )
+                        if last_page:
+                            self.current_page = 0
 
     def read_files_from_folder(self, folder, extension="ch8") -> list[str]:
         """Get all file names from folder with required format"""
